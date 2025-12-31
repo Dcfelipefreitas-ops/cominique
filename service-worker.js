cc8420d6ef268ed53a8d3cb5a9a6c8d74baf6da0
Dcfelipefreitas-ops <Dc.felipefreitas@hotmail.com>
2025-12-31 10:44:04 -0300

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = "teleprompter-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.json",
+        "./ffmpeg.min.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
